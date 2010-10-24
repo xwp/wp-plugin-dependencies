1105eeb8ee6b3fc2ef1e5a21b628c41aa5854089
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-24 21:51:39 +0000

    Release version 1.0
    git-svn-id: http://plugins.svn.wordpress.org/plugin-dependencies/trunk@303777 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-dependencies.php
@@ -0,0 +1,258 @@
+<?php
+/*
+Plugin Name: Plugin Dependencies
+Version: 1.0
+Description: Prevent activating plugins that don't have all their dependencies satisfied
+Author: scribu
+Author URI: http://scribu.net/
+Plugin URI: http://scribu.net/wordpress/plugin-dependencies
+Text Domain: plugin-dependencies
+Domain Path: /lang
+
+
+Copyright (C) 2010 Cristi Burcă (scribu@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+( at your option ) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class Plugin_Dependencies {
+
+	function init() {
+		add_action( 'load-plugins.php', array( __CLASS__, '_init' ) );
+		add_action( 'extra_plugin_headers', array( __CLASS__, 'extra_plugin_headers' ) );
+		add_filter( 'plugin_action_links', array( __CLASS__, 'plugin_action_links' ), 10, 4 );
+	}
+
+	function _init() {
+		load_plugin_textdomain( 'plugin-dependencies', '', dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+
+		if ( isset( $_REQUEST['action'] ) && 'deactivate' == $_REQUEST['action'] )
+			self::deactivate_cascade( (array) $_REQUEST['plugin'] );
+
+		add_action( 'admin_notices', array( __CLASS__, 'admin_notices' ) );
+		add_action( 'admin_print_styles', array( __CLASS__, 'admin_print_styles' ) );
+		add_action( 'admin_footer', array( __CLASS__, 'admin_footer' ) );
+	}
+
+	private static $active_plugins;
+	private static $deactivate_cascade;
+
+	private function deactivate_cascade( $to_deactivate ) {
+		$hierarchy = self::get_dependency_hierarchy();
+
+		self::$active_plugins = self::get_dependency_parents();
+		self::$deactivate_cascade = array();
+
+		self::_cascade( $to_deactivate );
+
+		deactivate_plugins( self::$deactivate_cascade );
+
+		set_transient( 'pd_deactivate_cascade', self::$deactivate_cascade );
+	}
+
+	private function _cascade( $to_deactivate ) {
+		$found = array();
+		foreach ( self::$active_plugins as $dep => $deps ) {
+			if ( empty( $deps ) )
+				continue;
+
+			if ( count( array_intersect( $to_deactivate, $deps ) ) )
+				$found[] = $dep;
+		}
+
+		$found = array_diff( $found, self::$deactivate_cascade ); // prevent endless loop
+		if ( empty( $found ) )
+			return;
+
+		self::$deactivate_cascade = array_merge( self::$deactivate_cascade, $found );
+
+		self::_cascade( $found );
+	}
+
+	// parent => children
+	private function get_dependency_hierarchy() {
+		$all_plugins = get_plugins();
+
+		$r = array();
+		foreach ( $all_plugins as $dep => $plugin_data ) {
+			foreach ( self::get_dependencies( $plugin_data ) as $parent )
+				$r[ $parent ][] = $dep;
+		}
+
+		return $r;
+	}
+
+	// child => parents
+	private function get_dependency_parents( $active = true ) {
+		if ( $active )
+			$plugins = self::get_active_plugins();
+		else
+			$plugins = get_plugins();
+
+		$r = array();
+		foreach ( $plugins as $dep => $plugin_data ) {
+			$r[ $dep ] = self::get_dependencies( $plugin_data );
+		}
+
+		return $r;
+	}
+
+	private function get_active_plugins() {
+		$all_plugins = get_plugins();
+
+		$active = array();
+		foreach ( $all_plugins as $dep => $plugin_data ) {
+			if ( !is_plugin_active( $dep ) && !is_plugin_active_for_network( $dep ) )
+				continue;
+
+			$active[ $dep ] = $plugin_data;
+		}
+
+		return $active;
+	}
+
+	function admin_notices() {
+		if ( !isset( $_REQUEST['deactivate'] ) )
+			return;
+
+		$deactivate_cascade = get_transient('pd_deactivate_cascade');
+
+		if ( empty( $deactivate_cascade ) )
+			return;
+
+		echo 
+		html( 'div', array( 'class' => 'updated' ), html( 'p',
+			__( 'The following plugins have also been deactivated:', 'plugin-dependencies' ),
+			self::generate_dep_list( $deactivate_cascade )
+		) );
+
+		delete_transient( 'pd_deactivate_cascade' );
+	}
+
+
+	function admin_print_styles() {
+?>
+<style type="text/css">
+.dep-list li { list-style: disc inside none }
+span.deps li { color: #BC0B0B }
+</style>
+<?php
+	}
+
+	# http://core.trac.wordpress.org/changeset/15944
+	function admin_footer() {
+		$all_plugins = get_plugins();
+
+		$hash = array();
+		foreach ( $all_plugins as $file => $data ) {
+			$name = isset( $data['Name'] ) ? $data['Name'] : $file;
+			$hash[ $name ] = sanitize_title( $name );
+		}
+
+?>
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+	var hash = <?php echo json_encode( $hash ); ?>
+
+	$('table.widefat tbody tr').not('.second').each(function() {
+		var $self = $(this),
+			title = $self.find('.plugin-title').text();
+
+		$self.attr('id', hash[title]);
+	});
+});
+</script>
+<?php
+	}
+
+	function extra_plugin_headers( $headers ) {
+		$headers['Dependencies'] = 'Dependencies';
+
+		return $headers;
+	}
+
+	function plugin_action_links( $actions, $plugin_file, $plugin_data, $context ) {
+		$deps = self::get_dependencies( $plugin_data );
+
+		if ( empty( $deps ) )
+			return $actions;
+
+		$unsatisfied = array();
+		foreach ( $deps as $dep ) {
+			if ( !is_plugin_active( $dep ) && !is_plugin_active_for_network( $dep ) )
+				$unsatisfied[] = $dep;
+		}
+
+		if ( empty( $unsatisfied ) )
+			return $actions;
+
+		unset( $actions['activate'] );
+
+		$actions['deps'] = __( 'Required plugins:', 'plugin-dependencies') . '<br>' . self::generate_dep_list( $unsatisfied );
+
+		return $actions;
+	}
+
+	private function generate_dep_list( $deps ) {
+		$all_plugins = get_plugins();
+
+		$dep_list = '';
+		foreach ( $deps as $dep ) {
+			if ( isset( $all_plugins[$dep] ) && isset( $all_plugins[$dep]['Name'] ) )
+				$dep = html( 'a', array( 'href' => '#' . sanitize_title( $all_plugins[$dep]['Name'] ) ), $all_plugins[$dep]['Name'] );
+			else
+				$dep = html( 'span', esc_html( $dep ) );
+
+			$dep_list .= html( 'li', $dep );
+		}
+
+		return html( 'ul', array( 'class' => 'dep-list' ), $dep_list );
+	}
+
+	private function get_dependencies( $plugin_data ) {
+		if ( empty( $plugin_data['Dependencies'] ) )
+			return array();
+
+		# http://core.trac.wordpress.org/attachment/ticket/15193/
+		if ( FALSE === strpos( $plugin_data['Dependencies'], '.php' ) )
+			return array();
+
+		return array_filter( preg_split( '/\s+/', $plugin_data['Dependencies'] ) );
+	}
+}
+
+Plugin_Dependencies::init();
+
+if ( ! function_exists( 'html' ) ):
+function html( $tag ) {
+	$args = func_get_args();
+
+	$tag = array_shift( $args );
+
+	if ( is_array( $args[0] ) ) {
+		$closing = $tag;
+		$attributes = array_shift( $args );
+		foreach ( $attributes as $key => $value ) {
+			$tag .= ' ' . $key . '="' . htmlspecialchars( $value, ENT_QUOTES ) . '"';
+		}
+	} else {
+		list( $closing ) = explode(' ', $tag, 2);
+	}
+
+	$content = implode('', $args);
+
+	return "<{$tag}>{$content}</{$closing}>";
+}
+endif;
+
---
+++ readme.txt
@@ -0,0 +1,63 @@
+=== Plugin Dependencies ===
+Contributors: scribu
+Donate link: http://scribu.net/wordpress
+Tags: plugin, dependency
+Requires at least: 3.0
+Tested up to: 3.1-alpha
+Stable tag: trunk
+
+Plugin dependency management
+
+== Description ==
+
+This meta-plugin allows regular plugins to define other plugins that they depend upon.
+
+**Defining dependencies**
+
+Example:
+
+`
+/*
+Plugin Name: Child Plugin
+Dependencies: parent-plugin/parent-plugin.php another-plugin.php
+*/
+`
+
+What this does:
+
+* Disables activation of *Child Plugin* until both *Parent Plugin* and *Another Plugin* are already activated.
+* When either *Parent Plugin* or *Another Plugin* are deactivated, *Child Plugin* will also be deactivated.
+
+Clarifications:
+
+* Dependencies are separated by spaces.
+* Each dependency is represented by a plugin basename.
+* A parent plugin can have dependencies of it's own.
+
+== Screenshots ==
+
+1. Activation prevention
+2. Cascade deactivation
+
+== Installation ==
+
+You can either use the built-in installer or do it manually:
+
+1. Unzip the "Plugin Dependencies" archive and put the folder into your plugins folder (/wp-content/plugins/).
+1. Activate the plugin from the Plugins menu.
+
+== Frequently Asked Questions ==
+
+= Error on activation: "Parse error: syntax error, unexpected..." =
+
+Make sure your host is running PHP 5. The only foolproof way to do this is to add this line to wp-config.php (after the opening `<?php` tag):
+
+`var_dump(PHP_VERSION);`
+<br>
+
+== Changelog ==
+
+= 1.0 =
+* initial release
+* [more info](http://scribu.net/wordpress/plugin-dependencies/pd-0-5.html)
+
